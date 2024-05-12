7a7a68629152a48c1ad0216ac88d97f42797eb33
Pavithran2 <pavithrank02@gmail.com>
2024-05-12 22:28:28 +0530

    ss

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-//xjjds
+//xjjdsx
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
